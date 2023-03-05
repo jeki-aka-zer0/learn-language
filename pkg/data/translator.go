bb2b9b806ded0210774b672ed680b321fe767008
Yevgeniy Zhukov <evgeniy.zhukov.dev@gmail.com>
2023-03-05 22:32:36 +0100

    WIP

--- pkg/data/translator.go
+++ pkg/data/translator.go
@@ -1,98 +1,93 @@
 package data
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"strings"
 )
 
-func Translate() string {
-	PROD := false
+type LangCode int
 
-	type Translation struct {
-		Detected_source_language string `json:"detected_source_language"`
-		Text                     string `json:"text"`
+const (
+	En LangCode = iota
+	Ru
+
+	translateUrl string = "https://api-free.deepl.com/v2/translate"
+)
+
+func (lc LangCode) String() string {
+	return []string{"EN", "RU"}[lc]
+}
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func NewTranslator(authKey string, client HTTPClient) *Translator {
+	return &Translator{
+		AuthKey: authKey,
+		client:  client,
 	}
-	type Translations struct {
-		Translations []Translation `json:"translations"`
-	}
+}
+
+type Translator struct {
+	AuthKey string
+	client  HTTPClient
+}
+
+type Translation struct {
+	DetectedSourceLanguage string `json:"detected_source_language"`
+	Text                   string `json:"text"`
+}
+
+type Translations struct {
+	Translations []Translation `json:"translations"`
+}
+
+func (t *Translator) Translate(sourceLang, targetLang LangCode, text string) (*Translations, error) {
 	var resp *http.Response
 
-	if PROD {
-		// Set up the request data
-		apiURL := "https://api-free.deepl.com/v2/translate"
-		authKey := "XXX"
-		sourceLang := "EN"
-		text := "Hello, world!"
-		targetLang := "RU"
+	// &http.Client{}
+	//sourceLang = En
+	//targetLang = Ru
+	//text = "Hello, world!"
 
-		// Set up the request body
-		data := url.Values{}
-		data.Set("source_lang", sourceLang)
-		data.Set("text", text)
-		data.Set("target_lang", targetLang)
-		body := strings.NewReader(data.Encode())
+	// Set up the request body
+	data := url.Values{}
+	data.Set("source_lang", sourceLang.String())
+	data.Set("text", text)
+	data.Set("target_lang", targetLang.String())
+	body := strings.NewReader(data.Encode())
 
-		// Set up the request
-		req, err := http.NewRequest("POST", apiURL, body)
-		if err != nil {
-			panic(err)
-		}
+	// Set up the request
+	req, err := http.NewRequest("POST", translateUrl, body)
+	if err != nil {
+		return nil, err
+	}
 
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Set("Authorization", "DeepL-Auth-Key "+authKey)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Authorization", "DeepL-Auth-Key "+t.AuthKey)
 
-		// Send the request
-		client := &http.Client{}
-		resp, err = client.Do(req)
-		if err != nil {
-			panic(err)
-		}
+	// Send the request
+	resp, err = t.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
 
-		defer resp.Body.Close()
+	defer resp.Body.Close()
 
-		// Check the status code
-		if resp.StatusCode != http.StatusOK {
-			panic(fmt.Errorf("error: %s", resp.Status))
-		}
-	} else {
-		mockedRes := Translations{
-			Translations: []Translation{
-				{
-					Detected_source_language: "EN",
-					Text:                     "Здравствуй, мир!!!",
-				},
-			},
-		}
-
-		// Marshal a JSON-encoded version of mockedRes
-		b, err := json.Marshal(mockedRes)
-		if err != nil {
-			log.Panic("Error reading a mockedRes from mocked client", err)
-		}
-
-		resp = &http.Response{Body: ioutil.NopCloser(bytes.NewBuffer(b))}
+	// Check the status code
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("cannot translate, status code is: %s", resp.Status)
 	}
 
 	post := &Translations{}
-	derr := json.NewDecoder(resp.Body).Decode(post)
-	if derr != nil {
-		panic(derr)
+	err = json.NewDecoder(resp.Body).Decode(post)
+	if err != nil {
+		return nil, err
 	}
 
-	for _, a := range post.Translations {
-		fmt.Println("Text:", a.Text)
-		fmt.Println("Detected_source_language:", a.Detected_source_language)
-	}
-
-	// fmt.Println("Title:", post.Title)
-	// fmt.Println("Body:", post.Body)
-	// fmt.Println("UserId:", post.UserId)
-
-	return "done"
+	return post, nil
 }
--- pkg/data/translator_test.go
+++ pkg/data/translator_test.go
@@ -1,21 +1,61 @@
 package data
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+)
+
+type MockClient struct {
+	DoFunc func(req *http.Request) (*http.Response, error)
+}
+
+var (
+	// GetDoFunc fetches the mock client's `Do` func
+	GetDoFunc func(req *http.Request) (*http.Response, error)
+)
+
+// Do is the mock client's `Do` func
+func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
+	return GetDoFunc(req)
+}
 
 func TestTranslate(t *testing.T) {
 	tests := []struct {
-		name string
-		want string
+		name     string
+		wantResp *Translations
+		wantErr  error
 	}{
 		{
 			name: "test",
-			want: "done",
+			wantResp: &Translations{
+				Translations: []Translation{{
+					DetectedSourceLanguage: "EN",
+					Text:                   "Здравствуй, мир!",
+				}},
+			},
+			wantErr: nil,
 		},
 	}
+
+	// build response JSON
+	json := `{"translations": [{"detected_source_language": "EN","text": "Здравствуй, мир!"}]}`
+	// create a new reader with that JSON
+	GetDoFunc = func(*http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: 200,
+			Body:       io.NopCloser(bytes.NewReader([]byte(json))),
+		}, nil
+	}
+
+	SUT := NewTranslator("TEST", &MockClient{})
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Translate(); got != tt.want {
-				t.Errorf("Translate() = %v, want %v", got, tt.want)
+			got, err := SUT.Translate(En, Ru, "Hello, world!")
+
+			if got.Translations[0].Text != tt.wantResp.Translations[0].Text || err != tt.wantErr {
+				t.Errorf("Translate() = %v, %v, want %v, %v", got, err, tt.wantResp, tt.wantErr)
 			}
 		})
 	}
